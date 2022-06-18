a418d3fe4b046047311962bc473a304fc0ceb254
“Elie <elie.maj@hotmail.com>
2022-06-18 19:19:08 +0300

    fix: restructure database

--- backend/database/migrations/2022_6_18_000006_create_answers_table.php
+++ backend/database/migrations/2022_6_18_000006_create_answers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOptionsTable extends Migration
+class CreateAnswersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateOptionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('options', function (Blueprint $table) {
+        Schema::create('asnwers', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('question_id');
             $table->foreign('question_id')->references('id')->on('questions');
-            $table->string('option');
+            $table->string('answer');
         });
     }
 
@@ -28,6 +28,6 @@ class CreateOptionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('options');
+        Schema::dropIfExists('asnwers');
     }
 }
