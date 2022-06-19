aa74d583ff05bf03435cab8475913e72b7a9224b
“Elie <elie.maj@hotmail.com>
2022-06-20 01:44:19 +0300

    add: login conditions to components

--- frontend-V2/src/App.js
+++ frontend-V2/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Questions from './Questions';
-
+import Admin from './Admin';
+import Navbar from './Navbar';
+import React, { useState } from 'react';
 function App() {
+  const [reload, setReload] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div id="body">
       <div className="title">hey there</div>
-      <Questions />
+      <Navbar setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
+      <Questions reload={reload} setReload={setReload} isLoggedIn={isLoggedIn} />
+      {isLoggedIn && <Admin setReload={setReload} />}
     </div>
   )
 }
